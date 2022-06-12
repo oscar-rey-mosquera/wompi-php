553ad6ff334d21694313b600f7001b96336af77e
Oscar Rey <oscarhumberto1311@gmail.com>
2022-06-12 17:46:17 -0500

    Agregar github actions

--- tests/env.example.php
+++
@@ -1,7 +0,0 @@
-<?php 
-
-$env = [
-    'public_key' => '',
-    'private_key' => '',
-    'private_event_key' => ''
-  ];
---
+++ tests/env.php
@@ -0,0 +1,7 @@
+<?php 
+
+$env = [
+    'public_key' => $_ENV['WOMPI_PUBLIC_KEY'],
+    'private_key' => $_ENV['WOMPI_PRIVATE_KEY'],
+    'private_event_key' => $_ENV['WOMPI_PRIVATE_EVENT_KEY']
+  ];
